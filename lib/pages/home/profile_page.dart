bbcbea29d532926800d0db232402612bd283f3c5
adiilam <adiilham878@gmail.com>
2024-08-14 22:05:37 +0700

    Profile Page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,3 +1,4 @@
+// import 'package:flutter/widgets.dart';
 import 'package:kopkarsi/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -7,22 +8,174 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget header() {
-      return Container(
-        padding: EdgeInsets.all(defaultMargin),
-        color: primaryColor,
-        child: const Column(
-          children: [
-            Row(
+      return AppBar(
+        backgroundColor: primaryColor,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(
+              defaultMargin,
+            ),
+            child: Row(
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage(
+                ClipOval(
+                  child: Image.asset(
                     'assets/image_profil.png',
+                    width: 65,
                   ),
-                  radius: 20,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Ahmad Habibi',
+                        style: backgroundTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        '7474 - Quality Control',
+                        style: backgroundTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: regular,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, '/sign-in', (route) => false);
+                  },
+                  child: Image.asset(
+                    'assets/button_exit.png',
+                    width: 20,
+                  ),
                 ),
               ],
             ),
+          ),
+        ),
+      );
+    }
+
+    Widget menuItem(String text, String iconPath) {
+      return Container(
+        margin: const EdgeInsets.only(
+          top: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              iconPath,
+              width: 24,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Text(
+              text,
+              style: titleTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: regular,
+              ),
+            ),
+            const Spacer(),
+            Icon(
+              Icons.chevron_right,
+              color: titleColor,
+            ),
           ],
+        ),
+      );
+    }
+
+    // Widget content() {
+    //   return Expanded(
+    //     child: Container(
+    //       width: double.infinity,
+    //       margin: const EdgeInsets.only(top: 50),
+    //       padding: EdgeInsets.symmetric(
+    //         horizontal: defaultMargin,
+    //       ),
+    //       decoration: BoxDecoration(
+    //         color: backgroundColor,
+    //         borderRadius: BorderRadius.circular(30),
+    //       ),
+    //       child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //         crossAxisAlignment: CrossAxisAlignment.end,
+    //         children: [
+    //           const SizedBox(
+    //             height: 50,
+    //           ),
+    //           Row(
+    //             children: [
+    //               Image.asset(
+    //                 'assets/icon_biodata.png',
+    //                 width: 24,
+    //               ),
+    //               const SizedBox(
+    //                 width: 20,
+    //               ),
+    //               menuItem(
+    //                 'Biodata',
+    //               )
+    //             ],
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   );
+    // }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: 390,
+          height: 576,
+          margin: const EdgeInsets.only(top: 50),
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              menuItem(
+                'Biodata',
+                'assets/icon_biodata.png',
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              menuItem(
+                'Keamanan',
+                'assets/icon_security.png',
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              menuItem(
+                'Tentang',
+                'assets/icon_tentang.png',
+              ),
+            ],
+          ),
         ),
       );
     }
@@ -32,6 +185,7 @@ class ProfilePage extends StatelessWidget {
       body: ListView(
         children: [
           header(),
+          content(),
         ],
       ),
     );
